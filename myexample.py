298f97d1e957d99aabe0f393d9dca4274ba97395
Sahaj Parmar <sahajrajput369@gmial.com>
2024-02-07 17:53:56 +0530

    add 3 files

---
+++ myexample.py
@@ -0,0 +1,24 @@
+
+#[:- Numbers & Variables -:]
+
+
+#print(1+2)
+#print("hello world")
+#print(1/2)
+#print(1-2)
+#print(3*3)
+#print(2**3)
+#print(2+10*10+3)
+#print((2+10)*(10+3))
+
+#a = 10
+#a = a + a
+#print(a)
+
+#puppies = 6
+#weight = 2
+
+#total = puppies * weight
+#print(total)
+
+
---
+++ string_1.py
@@ -0,0 +1,47 @@
+
+#print(len("Hello World"))
+
+#mystring1 = "Hello World"
+
+#print(mystring1[1])
+
+
+#[:-SLICING-:]
+
+#mystring2 = "abcdefghijk"
+
+#print(mystring2[0:3])
+
+#print(mystring2[4:])
+
+#print(mystring2[0:7:3])
+
+#print(mystring2[::2])
+
+#print("Hello " + mystring2)
+
+#[:-Upper Case-:]
+
+#print(mystring1.upper())
+
+#[:-Lower Case-:]
+
+#print(mystring1.lower())
+
+#[:-Spliting-:]
+
+#print(mystring1.split())
+
+#print(mystring1.split('o'))
+
+#[:-Format Method-:]
+
+username = "Sammy"
+
+color = "blue"
+
+print("The {} favorite is {}.".format(username,color))
+
+#f string literals!!
+
+print(f"The {username} chose {color}")
